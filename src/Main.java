dfbd46a1195abaa4791aa3d362b58fd9be27ea77
kyloc74 <kyloc74@gmail.com>
2023-01-02 02:56:49 +0000

    create class that generates cryptographic hash for each block

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
 import java.util.Date;
+import java.security.MessageDigest;
+
+public class stringUtil {
+    //Applies sha256 to a string and returns the result
+    public static  String applySha256(String input){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            //Applies sha 256 to our input
+            byte[] hash = digest.digest(input.getBytes("Utf-8"));
+            StringBuffer hexString = new StringBuffer(); //This will contain a hexadecimal hash
+
+            for (int i = 0; i < hash.length; i++){
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
 
 public class Block {
 
