fdbd9e24f38e2e702698244ae629d3ecafcba096
kyloc74 <kyloc74@gmail.com>
2023-01-02 17:31:05 +0000

    Add a comment to highlight the test for the mining system

--- src/NoobChain.java
+++ src/NoobChain.java
@@ -5,7 +5,7 @@ public class NoobChain {
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
     public static int difficulty = 5;
 
-
+   //This is a test for the mining system
     public static void main(String[] args) {
         //add our blocks to the blockchain ArrayList:
 
