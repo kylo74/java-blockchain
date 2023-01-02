511324e6ae923c1828f0808ef91155c227af66cd
kyloc74 <kyloc74@gmail.com>
2023-01-02 02:32:22 +0000

    create the class block that makes up the blockchain

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Date;
+
+public class Block {
+
+    public String hash;
+    public String previousHash;
+    private String data; //The data will be a simple message
+    private long timeStamp; //number of milliseconds since 1/1/1970
+
+    //This creates blocks
+    public Block(String data, String previousHash){
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
     }
 }
